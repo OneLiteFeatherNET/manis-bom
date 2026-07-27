64a49b15e2f93202403fd74cf5ab8a89f556bfbe
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-07-27 20:36:53 +0200

    fix(deps): update aonyx to 0.8.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            version("aonyx", "0.8.1")
+            version("aonyx", "0.8.2")
             version("hibernate.bom", "7.4.5.Final")
             version("cloudnet.bom", "4.0.0-RC16")
 
